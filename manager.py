52ca44c231d34b666f5c1c5fc56177fe824ff4b7
Alex Carrega <alessandro.carrega@gmail.com>
2025-05-22 16:00:05 +0200

    Automatic commit with GitManager

--- manager.py
+++ manager.py
@@ -18,6 +18,7 @@ def manager(*, config: "c" = "manager.toml"):  # noqa: F821
 
     for input_file, output_file in settings.pcap.files.items():
         logger.info(f"Input file: {input_file}")
+
         packets = rdpcap(input_file)
         modified_packets = []
 
@@ -25,7 +26,7 @@ def manager(*, config: "c" = "manager.toml"):  # noqa: F821
         for packet in packets:
             pkt_idx += 1
             if packet.haslayer(IP):
-                logger.info("{pkt_idx} IP src {packet[IP].src} dst {packet[IP].dst}")
+                logger.info(f"{pkt_idx} IP src {packet[IP].src} dst {packet[IP].dst}")
                 for pkt_dir in pkt_dirs:
                     for ip_pattern, ip_new in settings.ip.replace[pkt_dir].items():
                         if re.search(ip_pattern, getattr(packet[IP], pkt_dir)):
@@ -34,7 +35,7 @@ def manager(*, config: "c" = "manager.toml"):  # noqa: F821
                             logger.success(f"{pkt_idx} IP {pkt_dir} {ip_pattern} => {ip_new}")
             for l4_proto in l4_protos:
                 if packet.haslayer(l4_protos):
-                    del packet[l4_protos].chksum  # For L4 Proto packets, remove the checksum for recalculation
+                    del packet[l4_proto].chksum  # For L4 Proto packets, remove the checksum for recalculation
             remove_packet = False
             for pkt_dir in pkt_dirs:
                 for _, ip_pattern in settings.ip.delete[pkt_dir].items():
