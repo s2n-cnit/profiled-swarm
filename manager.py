f7330d8a71766cff55dab11fae6cb481c9ff802b
Alex Carrega <alessandro.carrega@gmail.com>
2025-05-23 20:39:09 +0200

    Automatic commit with GitManager

--- manager.py
+++ manager.py
@@ -9,6 +9,17 @@ pkt_dirs = ["src", "dst"]
 l4_protos = [TCP, UDP]
 
 
+def inverse_pkt_dir(pkt_dir: str) -> str:
+    return pkt_dirs[0] if pkt_dir == pkt_dirs[1] else pkt_dirs[1]
+
+
+def get_last_reply_ip(buffer_ips: list, random_values: list) -> str:
+    if len(buffer_ips) > 0:
+        return buffer_ips.pop()
+    else:
+        return random.choices(random_values)[0]
+
+
 def manager(*, config: "c" = "manager.toml"):  # noqa: F821
     """PCAP file Managers
 
@@ -23,6 +34,8 @@ def manager(*, config: "c" = "manager.toml"):  # noqa: F821
         packets = rdpcap(input_file)
         modified_packets = []
 
+        buffer_ips = {pkt_dirs[0]: [], pkt_dirs[1]: []}
+
         pkt_idx = 0
         for packet in packets:
             pkt_idx += 1
@@ -32,7 +45,16 @@ def manager(*, config: "c" = "manager.toml"):  # noqa: F821
                     for _, ip_pattern in settings.ip.replace[pkt_dir].target.items():
                         if re.search(ip_pattern, getattr(packet[IP], pkt_dir)):
                             ip_new = random.choices(settings.ip.replace[pkt_dir].random.values())[0]
+                            buffer_ips[pkt_dir].insert(0, ip_new)
                             setattr(packet[IP], pkt_dir, ip_new)
+                            del packet[IP].chksum  # Remove the checksum so Scapy recalculates it
+                            logger.success(f"{pkt_idx} IP {pkt_dir} {ip_pattern} => {ip_new}")
+                    for _, ip_pattern in settings.ip.reply_last[pkt_dir].items():
+                        if re.search(ip_pattern, getattr(packet[IP], pkt_dir)):
+                            inv_pkt_dir = inverse_pkt_dir(pkt_dir)
+                            ip_new = get_last_reply_ip(buffer_ips[inv_pkt_dir],
+                                                       settings.ip.replace[pkt_dir].random.values())
+                            setattr(packet[IP], inv_pkt_dir, ip_new)
                             del packet[IP].chksum  # Remove the checksum so Scapy recalculates it
                             logger.success(f"{pkt_idx} IP {pkt_dir} {ip_pattern} => {ip_new}")
             for l4_proto in l4_protos:
@@ -49,4 +71,4 @@ def manager(*, config: "c" = "manager.toml"):  # noqa: F821
                 modified_packets.append(packet)
 
         wrpcap(output_file, modified_packets)
-        logger.success(f"Output file: {input_file}")
+        logger.success(f"Output file: {output_file}")
