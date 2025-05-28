d68e665cbc26f13eb040ba2db102eebf17bd4133
Alex Carrega <alessandro.carrega@unige.it>
2025-05-28 11:33:30 +0200

    Fix

--- fabfile.py
+++ fabfile.py
@@ -1,93 +1,146 @@
-from fabric.api import cd, env, run
+from fabric import Connection, ThreadingGroup, task
 
-code_dir = "$HOME/profiled-swarm"
-
-
-def requirements():
-    if run("command -v python3.12").failed:
-        run("curl -s https://raw.githubusercontent.com/markelog/ec-install/master/scripts/install.sh | sh")
-        run("ec python@3.12")
-        run("pip install poetry")
+set_ue_a = [str(x) + ".ueransim.5g.horse.s2n-cnit" for x in range(1, 6)]
+set_ue_b = [str(x) + ".ueransim.5g.horse.s2n-cnit" for x in range(6, 11)]
 
 
-def deploy():
-    if run("command -v git").failed:
-        run("sudo apt install -y git")
-    if run(f"test -d {code_dir}").failed:
-        run(f"git clone https://github.com/s2n-cnit/profiled-swarm.git {code_dir}")
-    with cd(code_dir):
-        run("poetry install")
-
-
-def generate(profile: str):
-    with cd(code_dir):
-        run("sudo su")
-        run(f"poetry run python profiled-swarm.py -p profile.{profile}")
-
-
-# HPING3 alternative solution
-
-set_ue_a = [x + ".ueransim.5g.horse.s2n-cnit" for x in range(1, 6)]
-set_ue_b = [x + ".ueransim.5g.horse.s2n-cnit" for x in range(6, 11)]
-
-env.roledefs = {
-    'streaming_a': set_ue_a,
-    'streaming_b': set_ue_b,
-    'streaming_c': set_ue_a,
-    'streaming_d': set_ue_b,
-    'dns': set_ue_a + set_ue_b
+kwargs = {
+    "password": 'ubuntu',
 }
 
-
-def hping3_gen(dst_ip: str, dst_port: int, packet_size: int, packet_count: int, speed: str, protocol: str):
-    run(f"hping3 -c {packet_count} {speed} {protocol} -p {dst_port} {dst_ip} -d {packet_size}")
-
-
-def hping3_gen_streaming_a_normal():
-    hping3_gen(dst_ip="192.168.130.45", dst_port=80, packet_size=200,
-               packet_count=20000, speed="-i u100", protocol="-2")
+groups = dict(normal=ThreadingGroup(*set_ue_a, user="ubuntu", connect_kwargs=kwargs),
+              attack=ThreadingGroup(*set_ue_b, user="ubuntu", connect_kwargs=kwargs))
 
 
-def hping3_gen_streaming_b_normal():
-    hping3_gen(dst_ip="192.168.130.45", dst_port=443, packet_size=200,
-               packet_count=20000, speed="-i u100", protocol="-2")
+def set_groups(group_key: str | list):
+    if isinstance(group_key, str):
+        return [group_key]
+    return group_key
 
 
-def hping3_gen_streaming_c_normal():
-    hping3_gen(dst_ip="192.168.130.19", dst_port=80, packet_size=200,
-               packet_count=20000, speed="-i u100", protocol="-2")
+def hping3_gen(dst_ip: str, dst_port: int, packet_size: int, packet_count: int, speed: str, protocol: str) -> str:
+    return f"hping3 -c {packet_count} {speed} {protocol} -p {dst_port} {dst_ip} -d {packet_size}"
 
 
-def hping3_gen_streaming_d_normal():
-    hping3_gen(dst_ip="192.168.130.19", dst_port=443, packet_size=200,
-               packet_count=20000, speed="-i u100", protocol="-2")
+core_upf_ip = "192.168.130.45"
+edge_upf_ip = "192.168.130.19"
+dns_ip = "192.168.130.17"
+packet_size = 200
+
+streaming_port_a = 80
+streaming_port_b = 443
+dns_port = 53
+
+packet_size = 200
+packet_count = 10 * 1000 * 1000
+
+speed_normal = "-i u1000"
+speed_attack = "-i u100"
+
+proto_tcp = "-2"
 
 
-def hping3_gen_streaming_a_attack():
-    hping3_gen(dst_ip="192.168.130.45", dst_port=80, packet_size=200,
-               packet_count=200000, speed="--faster", protocol="-2")
+@task
+def requirements(c: Connection) -> None:
+    for g in set_groups(['normal', 'attack']):
+        groups[g].sudo("apt install hping3")
 
 
-def hping3_gen_streaming_b_attack():
-    hping3_gen(dst_ip="192.168.130.45", dst_port=443, packet_size=200,
-               packet_count=200000, speed="--faster", protocol="-2")
+@task
+def hping3_gen_streaming_a_normal_core(c: Connection, dst_ip: str = core_upf_ip, dst_port: int = streaming_port_a,
+                                       packet_size: int = packet_size, packet_count: int = packet_count,
+                                       speed: str = speed_normal,
+                                       ue_group: str | list = "normal"):
+    for g in set_groups(ue_group):
+        groups[g].sudo(hping3_gen(dst_ip=dst_ip, dst_port=dst_port, packet_size=packet_size,
+                                  packet_count=packet_count, speed=speed, protocol=proto_tcp))
 
 
-def hping3_gen_streaming_c_attack():
-    hping3_gen(dst_ip="192.168.130.19", dst_port=80, packet_size=200,
-               packet_count=200000, speed="--faster", protocol="-2")
+@task
+def hping3_gen_streaming_b_normal_core(c: Connection, dst_ip: str = core_upf_ip, dst_port: int = streaming_port_b,
+                                       packet_size: int = packet_size, packet_count: int = packet_count,
+                                       speed: str = speed_normal,
+                                       ue_group: str | list = "normal"):
+    for g in set_groups(ue_group):
+        groups[g].sudo(hping3_gen(dst_ip=dst_ip, dst_port=dst_port, packet_size=packet_size,
+                                  packet_count=packet_count, speed=speed, protocol=proto_tcp))
 
 
-def hping3_gen_streaming_d_attack():
-    hping3_gen(dst_ip="192.168.130.19", dst_port=443, packet_size=200,
-               packet_count=200000, speed="-faster", protocol="-2")
+@task
+def hping3_gen_streaming_a_normal_edge(c: Connection, dst_ip: str = edge_upf_ip, dst_port: int = streaming_port_a,
+                                       packet_size: int = packet_size, packet_count: int = packet_count,
+                                       speed: str = speed_normal,
+                                       ue_group: str | list = "normal"):
+    for g in set_groups(ue_group):
+        groups[g].sudo(hping3_gen(dst_ip=dst_ip, dst_port=dst_port, packet_size=packet_size,
+                                  packet_count=packet_count, speed=speed, protocol=proto_tcp))
 
 
-def hping3_gen_dns_normal():
-    hping3_gen(dst_ip="192.168.130.17", dst_port=53, packet_size=200,
-               packet_count=20000, speed="-i u100", protocol="-2")
+@task
+def hping3_gen_streaming_b_normal_edge(c: Connection, dst_ip: str = edge_upf_ip, dst_port: int = streaming_port_b,
+                                       packet_size: int = packet_size, packet_count: int = packet_count,
+                                       speed: str = speed_normal,
+                                       ue_group: str | list = "normal"):
+    for g in set_groups(ue_group):
+        groups[g].sudo(hping3_gen(dst_ip=dst_ip, dst_port=dst_port, packet_size=packet_size,
+                                  packet_count=packet_count, speed=speed, protocol=proto_tcp))
 
 
-def hping3_gen_dns_attack():
-    hping3_gen(dst_ip="192.168.130.19", dst_port=53, packet_size=200,
-               packet_count=200000, speed="-faster", protocol="-2")
+@task
+def hping3_gen_streaming_a_attack_core(c: Connection, dst_ip: str = core_upf_ip, dst_port: int = streaming_port_a,
+                                       packet_size: int = packet_size, packet_count: int = packet_count,
+                                       speed: str = speed_attack,
+                                       ue_group: str | list = "attack"):
+    for g in set_groups(ue_group):
+        groups[g].sudo(hping3_gen(dst_ip=dst_ip, dst_port=dst_port, packet_size=packet_size,
+                                  packet_count=packet_count, speed=speed, protocol=proto_tcp))
+
+
+@task
+def hping3_gen_streaming_b_attack_core(c: Connection, dst_ip: str = core_upf_ip, dst_port: int = streaming_port_b,
+                                       packet_size: int = packet_size, packet_count: int = packet_count,
+                                       speed: str = speed_attack,
+                                       ue_group: str | list = "attack"):
+    for g in set_groups(ue_group):
+        groups[g].sudo(hping3_gen(dst_ip=dst_ip, dst_port=dst_port, packet_size=packet_size,
+                                  packet_count=packet_count, speed=speed, protocol=proto_tcp))
+
+
+@task
+def hping3_gen_streaming_a_attack_edge(c: Connection, dst_ip: str = edge_upf_ip, dst_port: int = streaming_port_a,
+                                       packet_size: int = packet_size, packet_count: int = packet_count,
+                                       speed: str = speed_attack,
+                                       ue_group: str | list = "attack"):
+    for g in set_groups(ue_group):
+        groups[g].sudo(hping3_gen(dst_ip=dst_ip, dst_port=dst_port, packet_size=packet_size,
+                                  packet_count=packet_count, speed=speed, protocol=proto_tcp))
+
+
+@task
+def hping3_gen_streaming_b_attack_edge(c: Connection, dst_ip: str = edge_upf_ip, dst_port: int = streaming_port_b,
+                                       packet_size: int = packet_size, packet_count: int = packet_count,
+                                       speed: str = speed_attack,
+                                       ue_group: str | list = "attack"):
+    for g in set_groups(ue_group):
+        groups[g].sudo(hping3_gen(dst_ip=dst_ip, dst_port=dst_port, packet_size=packet_size,
+                                  packet_count=packet_count, speed=speed, protocol=proto_tcp))
+
+
+@task
+def hping3_gen_dns_normal(c: Connection, dst_ip: str = dns_ip, dst_port: int = dns_port,
+                          packet_size: int = packet_size, packet_count: int = packet_count,
+                          speed: str = speed_normal,
+                          ue_group: str | list = "normal"):
+    for g in set_groups(ue_group):
+        groups[g].sudo(hping3_gen(dst_ip=dst_ip, dst_port=dst_port, packet_size=packet_size,
+                                  packet_count=packet_count, speed=speed, protocol=proto_tcp))
+
+
+@task
+def hping3_gen_dns_attack(c: Connection, dst_ip: str = dns_ip, dst_port: int = dns_port,
+                          packet_size: int = packet_size, packet_count: int = packet_count,
+                          speed: str = speed_attack,
+                          ue_group: str | list = "attack"):
+    for g in set_groups(ue_group):
+        groups[g].sudo(hping3_gen(dst_ip=dst_ip, dst_port=dst_port, packet_size=packet_size,
+                                  packet_count=packet_count, speed=speed, protocol=proto_tcp))
