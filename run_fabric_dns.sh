b6c4bc4a204fc7e4d095a27f7e25c9da1dc9d05f
Alex Carrega <alessandro.carrega@unige.it>
2025-10-07 15:21:10 +0200

    Automatic commit with GitManager

--- run_fabric_dns.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-fab -R dns hping3_gen_dns_$1 &
--- run_fabric_streaming.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-fab -R streaming_a hping3_gen_streaming_a_$1 &
-fab -R streaming_b hping3_gen_streaming_b_$1 &
-fab -R streaming_c hping3_gen_streaming_c_$1 &
-fab -R streaming_d hping3_gen_streaming_d_$1 &
