96cc5f2f91751de1c9acbf499c30fa6a45b15040
Alex Carrega <alessandro.carrega@unige.it>
2025-12-16 10:33:28 +0100

    Automatic commit with GitManager

--- profiled-swarm.py
+++ profiled-swarm.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env -S poetry -C /axc-mgmt/github/s2n-cnit/profiled-swarm run python
+#!/usr/bin/env -S uv --directory /axc-mgmt/github/s2n-cnit/profiled-swarm run python
 
 from clize import run
 from generator import generator
